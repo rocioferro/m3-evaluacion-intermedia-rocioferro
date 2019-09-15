ab9dd8d56316b7cdba022e7c87042f0f5f09be56
rocioferro <fer.rodriguezrocio@gmail.com>
2019-09-15 19:22:51 +0200

    Create docs folder and publish in github pages

---
+++ docs/precache-manifest.8dbdec14e806cccd0867b6b987d3a939.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d63662b9e7b6791b97556c682658232",
+    "url": "./index.html"
+  },
+  {
+    "revision": "783471ce90da40eb7662",
+    "url": "./static/css/main.e60518fb.chunk.css"
+  },
+  {
+    "revision": "965b8e3326e63c0e0841",
+    "url": "./static/js/2.04cde4b1.chunk.js"
+  },
+  {
+    "revision": "783471ce90da40eb7662",
+    "url": "./static/js/main.8a41fd82.chunk.js"
+  },
+  {
+    "revision": "9ccd92aae0494bd03866",
+    "url": "./static/js/runtime~main.9ba77c7f.js"
+  }
+]);
